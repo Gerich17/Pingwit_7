bf8b482316897b7304259069780da1812ebda71b
Gerich17 <gerich1751@mail.ru>
2023-09-10 18:56:09 +0200

    inicial commit

---
+++ src/main/java/pl/pingwit/App.java
@@ -0,0 +1,12 @@
+package pl.pingwit;
+
+/**
+  Good job,Kiryl!
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Good job,Kiryl!" );
+    }
+}
---
+++ src/main/java/pl/pingwit/HelloPingwit1.java
@@ -0,0 +1,7 @@
+package pl.pingwit;
+
+public class HelloPingwit1 {
+    public static void main(String[] args) {
+        System.out.println("Hello Pingwit1, " + args[0] );
+    }
+}
