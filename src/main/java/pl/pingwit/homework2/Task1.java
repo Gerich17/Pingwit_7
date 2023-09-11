2431a6ad85f8c277d850b90090c563a9411a0b21
Gerich17 <gerich1751@mail.ru>
2023-09-11 17:28:08 +0200

    Homework in progress

--- src/main/java/pl/pingwit/homework2/Task1.java
+++ src/main/java/pl/pingwit/homework2/Task1.java
@@ -1,8 +1,21 @@
 package pl.pingwit.homework2;
 
+import javax.swing.plaf.synth.SynthOptionPaneUI;
+
 public class Task1 {
     public static void main(String[] args) {
         System.out.println(Integer.MAX_VALUE);
         System.out.println(Integer.MIN_VALUE);
+        System.out.println(Byte.MAX_VALUE);
+        System.out.println(Byte.MIN_VALUE);
+        System.out.println(Long.MAX_VALUE);
+        System.out.println(Long.MIN_VALUE);
+        System.out.println(Double.MAX_VALUE);
+        System.out.println(Double.MIN_VALUE);
+        System.out.println(Float.MAX_VALUE);
+        System.out.println(Float.MIN_VALUE);
+
+
+
     }
 }
