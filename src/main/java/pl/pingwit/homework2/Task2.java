f2a7c99d631bdf7ce7a87c60ea402766af21b092
Gerich17 <gerich1751@mail.ru>
2023-09-11 17:39:53 +0200

    Homework in progress

--- src/main/java/pl/pingwit/homework2/Task2.java
+++ src/main/java/pl/pingwit/homework2/Task2.java
@@ -6,5 +6,19 @@ public class Task2 {
         // byte, short, int, float, double
         byte myParsedByte = Byte.parseByte("77");
         System.out.println(myParsedByte);
+        short myParsedShort = Short.parseShort("12587");
+        System.out.println(myParsedShort);
+        int myParsedInteger = Integer.parseInt("-78945627");
+        System.out.println(myParsedInteger);
+        float myParsedFloat = Float.parseFloat("123.6789");
+        System.out.println(myParsedFloat);
+        double myParsedDouble = Double.parseDouble("-8965.876655");
+        System.out.println(myParsedDouble);
+
+
+
+
+
+
     }
 }
