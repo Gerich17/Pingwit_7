4d0b69f4864958cabe1d71e52c53c074cd3f8ba0
Gerich17 <gerich1751@mail.ru>
2023-09-14 17:26:45 +0200

    Homework in progress

---
+++ src/main/java/pl/pingwit/homework3/Task4.java
@@ -0,0 +1,13 @@
+package pl.pingwit.homework3;
+
+public class Task4 {
+    public static void main(String[] args) {
+        int A = 147;
+        int B = 7;
+        int result = 0;
+        for (int i = 0; i < A; i++) {
+            result = result + B;
+        }
+        System.out.println(result);
+    }
+}
