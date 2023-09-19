2ed017c763a91163dae7d2b730879660fd30a7e9
Pavel Radkevich <pradkevich@eisgroup.com>
2023-09-19 21:21:51 +0300

    ДЗ3 проверка 1

--- src/main/java/pl/pingwit/homework3/SummaChisel.java
+++ src/main/java/pl/pingwit/homework3/SummaChisel.java
@@ -1,9 +1,10 @@
 package pl.pingwit.homework3;
 
+// плиз старайся называть классы по-английски, а не транслитом. здесь подойдет ProgressionSum
 public class SummaChisel {
     // Вычислить: 1+2+4+8+...+256
     public static void main(String[] args) {
-
+// kbiyzz gecnfz cnhjrf
         int sum = 0;
         for (int i = 1; i <= 256; i = i * 2) {
             sum += i;
