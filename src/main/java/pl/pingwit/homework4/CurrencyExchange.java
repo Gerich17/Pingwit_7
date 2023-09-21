ca657dc1db44445dfcc280b933cb9e6fd65ae657
Gerich17 <gerich1751@mail.ru>
2023-09-21 17:36:30 +0200

    Homework is not ready

---
+++ src/main/java/pl/pingwit/homework4/CurrencyExchange.java
@@ -0,0 +1,24 @@
+package pl.pingwit.homework4;
+
+import java.math.BigDecimal;
+import java.util.Scanner;
+
+public class CurrencyExchange {
+    public static void main(String[] args)
+    {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the required currency : ");
+        String currency = scanner.next();
+        System.out.println("Enter amount");
+        BigDecimal amount = new BigDecimal( scanner.next());
+
+        BigDecimal toEuroRate = new BigDecimal("40");
+        if (currency == "eur") {
+            BigDecimal result = amount.multiply(toEuroRate);
+            System.out.println("Pay " + result + " uah ");
+        } else if (currency == "uah") {
+            BigDecimal result = amount.divide(toEuroRate);
+            System.out.println("Pay " + result + " eur ");
+        }
+    }
+}
