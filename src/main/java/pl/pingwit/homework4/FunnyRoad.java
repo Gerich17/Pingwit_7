ef4625bd336cf58db1a327d308d41f4048081185
Gerich17 <gerich1751@mail.ru>
2023-09-22 19:04:59 +0200

    Homework is ready

--- src/main/java/pl/pingwit/homework4/FunnyRoad.java
+++ src/main/java/pl/pingwit/homework4/FunnyRoad.java
@@ -8,38 +8,30 @@ public class FunnyRoad {
     Определить время старта программы можно при помощи System.currentTimeMillis()
     - это вернет вам текущее время в миллисекундах. */
     private static final String FUNNY_ROAD = "||  |  ||";
+
     public static void main(String[] args) {
-       // while (true); - цикл будет бесконечным
+        // while (true); - цикл будет бесконечным
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter travel time in seconds : ");
         int roadTime = scanner.nextInt(); // в этой переменной будет храниться число
         long startTime = System.currentTimeMillis(); // кол-во мл/с от начала отсчета
         long finishTime = startTime + roadTime * 1000L;
 
-         while (System.currentTimeMillis() < finishTime) {
+        while (System.currentTimeMillis() < finishTime) {
 
-             for (int i = 0; i < 10; i++) {
-                 for (int j = 0; j < i; j++) {
-                     System.out.print(" ");
-                 }
-                 System.out.println(FUNNY_ROAD);
-             }
-             for (int i = 10; i >= 0; i--) {
-                 for (int j = i; j >= 0; j--) {
-                     System.out.print(" ");
-                 }
-                 System.out.println(FUNNY_ROAD);
-             }
-         }
-
-
-
-
-
-
-
-
-
+            for (int i = 0; i < 10; i++) {
+                for (int j = 0; j < i; j++) {
+                    System.out.print(" ");
+                }
+                System.out.println(FUNNY_ROAD);
+            }
+            for (int i = 10; i >= 0; i--) {
+                for (int j = i; j >= 0; j--) {
+                    System.out.print(" ");
+                }
+                System.out.println(FUNNY_ROAD);
+            }
+        }
 
 
     }
